e85a3c8b24836d3c2e88665adfb0ca9eab970c09
type <type>
2023-11-21 19:36:37 -0800

    Added basic startup code

---
+++ TestLib/include/TestLib/Core/Core.h
@@ -0,0 +1,6 @@
+#pragma once
+
+
+namespace TestLib{
+    void print();
+};
---
+++ TestLib/src/TestLib/Core/Core.cpp
@@ -0,0 +1,8 @@
+#include <TestLib/Core/Core.h>
+#include <stdio.h>
+
+namespace TestLib{
+    void print(){
+        printf("This TestLib works and is able to find dependencies!!!\n");
+    }
+};
